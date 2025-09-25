00f538bb6070dde292e79fdf2d67e148e088b5db
nischal-qsystems <nischal.shakya@qsystems.ai>
2025-09-25 22:10:41 +0545

    fix: image location

--- web/utils/sidebar_logo.py
+++ web/utils/sidebar_logo.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 
 def add_sidebar_logo():
-    logo_img = Image.open("static/verizon_logo.png")
-    icon_img = Image.open("static/verizon_logo.png")
+    logo_img = Image.open("web/static/verizon_logo.png")
+    icon_img = Image.open("web/static/verizon_logo.png")
 
     st.logo(logo_img, icon_image=icon_img)
